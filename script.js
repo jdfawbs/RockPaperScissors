0dd08ecc157a5d055289e2ccc0b3a471b1ca28ec
jdfawbs <jdfawbs@gmail.com>
2025-01-29 22:08:37 +0000

    Initial comment pseudocode

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+console.log("Welcome to Rock - Paper - Scissors");
+
+//Create a function called getComputerChoice that returns a random choice from Rock, Paper or Scissors.
+
+//Create a function called getHumanChoise that prompts a user to enter a valid choice from Rock, Paper or Scissors.
+
+//Create 2 variables called humanScore and computerScore to store and keep track of the game score.
+
+//Create a function called playRound with 2 parameters (humanChoice, computerChoice)
+//Make the human choice lowercase
+//Display the coumpters choice in the console
+//Display the round result in the console
+//Increment the score of the winer to the variables humanScore or computerScore
+
+//Create a function call playGame that calls playRound
+//Play 5 rounds
+//Declare the winner of each round
+//Declare the winner of the game after 5 rounds or first to 3 rounds
